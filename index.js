316014d21ca024431ad900623d888f537b0e44b9
sharifclick <me.sharifahmed@gmail.com>
2018-12-04 19:22:56 +0600

    es6 transformation and modifications on main object

--- index.js
+++ index.js
@@ -43,131 +43,74 @@ class CanvasImage {
   }
 }
 
-var ColorThief = function () { };
+class ColorSwatches {
 
-/*
- * getColor(sourceImage[, quality])
- * returns {r: num, g: num, b: num}
- *
- * Use the median cut algorithm provided by quantize.js to cluster similar
- * colors and return the base color from the largest cluster.
- *
- * Quality is an optional argument. It needs to be an integer. 1 is the highest quality settings.
- * 10 is the default. There is a trade-off between quality and speed. The bigger the number, the
- * faster a color will be returned but the greater the likelihood that it will not be the visually
- * most dominant color.
- *
- * */
-ColorThief.prototype.getColor = function (sourceImage, quality) {
-  var palette = this.getPalette(sourceImage, 5, quality);
-  var dominantColor = palette[0];
-  return dominantColor;
-};
-
-
-/*
- * getPalette(sourceImage[, colorCount, quality])
- * returns array[ {r: num, g: num, b: num}, {r: num, g: num, b: num}, ...]
- *
- * Use the median cut algorithm provided by quantize.js to cluster similar colors.
- *
- * colorCount determines the size of the palette; the number of colors returned. If not set, it
- * defaults to 10.
- *
- * BUGGY: Function does not always return the requested amount of colors. It can be +/- 2.
- *
- * quality is an optional argument. It needs to be an integer. 1 is the highest quality settings.
- * 10 is the default. There is a trade-off between quality and speed. The bigger the number, the
- * faster the palette generation but the greater the likelihood that colors will be missed.
- *
- *
- */
-ColorThief.prototype.getPalette = function (sourceImage, colorCount, quality) {
-
-  if (typeof colorCount === 'undefined' || colorCount < 2 || colorCount > 256) {
-    colorCount = 10;
-  }
-  if (typeof quality === 'undefined' || quality < 1) {
-    quality = 10;
+  getHexSwatches(sourceImage, colorCount, quality) {
+    const palettes = this.getPalette(sourceImage, colorCount, quality);
+    return palettes.map((palette) => `#${palette.map((v) => parseInt(v).toString(16)).join('')}`)
   }
 
-  // Create custom CanvasImage object
-  var image = new CanvasImage(sourceImage);
-  var imageData = image.getImageData();
-  var pixels = imageData.data;
-  var pixelCount = image.getPixelCount();
+  getPalette(sourceImage, colorCount, quality) {
 
-  // Store the RGB values in an array format suitable for quantize function
-  var pixelArray = [];
-  for (var i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
-    offset = i * 4;
-    r = pixels[offset + 0];
-    g = pixels[offset + 1];
-    b = pixels[offset + 2];
-    a = pixels[offset + 3];
-    // If pixel is mostly opaque and not white
-    if (a >= 125) {
-      if (!(r > 250 && g > 250 && b > 250)) {
-        pixelArray.push([r, g, b]);
+    if (typeof colorCount === 'undefined' || colorCount < 2 || colorCount > 256) {
+      colorCount = 10;
+    }
+    if (typeof quality === 'undefined' || quality < 1) {
+      quality = 10;
+    }
+
+    // Create custom CanvasImage object
+    const image = new CanvasImage(sourceImage);
+    const imageData = image.getImageData();
+    const pixels = imageData.data;
+    const pixelCount = image.getPixelCount();
+
+    // Store the RGB values in an array format suitable for quantize function
+    const pixelArray = [];
+    for (let i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
+      offset = i * 4;
+      r = pixels[offset + 0];
+      g = pixels[offset + 1];
+      b = pixels[offset + 2];
+      a = pixels[offset + 3];
+      // If pixel is mostly opaque and not white
+      if (a >= 125) {
+        if (!(r > 250 && g > 250 && b > 250)) {
+          pixelArray.push([r, g, b]);
+        }
       }
     }
-  }
 
-  // Send array to quantize function which clusters values
-  // using median cut algorithm
-  var cmap = MMCQ.quantize(pixelArray, colorCount);
-  var palette = cmap ? cmap.palette() : null;
+    // Send array to quantize function which clusters values
+    // using median cut algorithm
+    const cmap = MMCQ.quantize(pixelArray, colorCount);
+    const palette = cmap ? cmap.palette() : null;
 
-  // Clean up
-  image.removeCanvas();
+    // Clean up
+    image.removeCanvas();
+    return palette;
+  };
 
-  return palette;
-};
-
-ColorThief.prototype.getColorFromUrl = function (imageUrl, callback, quality) {
-  sourceImage = document.createElement("img");
-  var thief = this;
-  sourceImage.addEventListener('load', function () {
-    var palette = thief.getPalette(sourceImage, 5, quality);
-    var dominantColor = palette[0];
-    callback(dominantColor, imageUrl);
-  });
-  sourceImage.src = imageUrl
-};
-
-
-ColorThief.prototype.getImageData = function (imageUrl, callback) {
-  xhr = new XMLHttpRequest();
-  xhr.open('GET', imageUrl, true);
-  xhr.responseType = 'arraybuffer'
-  xhr.onload = function (e) {
-    if (this.status == 200) {
-      uInt8Array = new Uint8Array(this.response)
-      i = uInt8Array.length
-      binaryString = new Array(i);
-      for (var i = 0; i < uInt8Array.length; i++) {
-        binaryString[i] = String.fromCharCode(uInt8Array[i])
+  getImageData(imageUrl, callback) {
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', imageUrl, true);
+    xhr.responseType = 'arraybuffer'
+    xhr.onload = function (e) {
+      if (this.status == 200) {
+        uInt8Array = new Uint8Array(this.response)
+        i = uInt8Array.length
+        binaryString = new Array(i);
+        for (var i = 0; i < uInt8Array.length; i++) {
+          binaryString[i] = String.fromCharCode(uInt8Array[i])
+        }
+        data = binaryString.join('')
+        base64 = window.btoa(data)
+        callback(`data:image/png;base64,${base64}`)
       }
-      data = binaryString.join('')
-      base64 = window.btoa(data)
-      callback("data:image/png;base64," + base64)
     }
-  }
-  xhr.send();
-};
-
-ColorThief.prototype.getColorAsync = function (imageUrl, callback, quality) {
-  var thief = this;
-  this.getImageData(imageUrl, function (imageData) {
-    sourceImage = document.createElement("img");
-    sourceImage.addEventListener('load', function () {
-      var palette = thief.getPalette(sourceImage, 5, quality);
-      var dominantColor = palette[0];
-      callback(dominantColor, this);
-    });
-    sourceImage.src = imageData;
-  });
-};
+    xhr.send();
+  };
+}
 
 
 
